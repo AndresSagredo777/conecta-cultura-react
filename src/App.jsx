254d5fdb30ca5195b131263f40ea55a31de2c29c
AndresSagredo777 <and.sagredo@duocuc.cl>
2026-09-23 11:52:28 -0300

    Iniciar proyecto react con vite

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Bienvenida from "./components/Bienvenida";
+import Cabecera from "./components/Cabecera";
+import TarjetaActividad from "./components/TarjetaActividad";
+import Navegacion from "./components/Navegacion"
+
+function App() {
+  return (
+    <>
+      <Cabecera />
+      <main className="container py-4">
+        <Bienvenida />
+        <div className="row g-4">
+          <div className="col-12 col-md-6 col-lg-4">
+            <TarjetaActividad />
+          </div>
+        </div>
+      </main>
+    </>
+  );
+} 
+
+export default App;
